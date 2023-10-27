5f2f7e4e7d121a4fc4af10fa79b4750382af607f
theslash84 <71232730+theslash84@users.noreply.github.com>
2023-10-26 22:06:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name  = "github-rg"
+    storage_account_name = "storageacctgithubtest"
+    container_name       = "tfstatecontainer"
+    key                  = "terraform.tfstat"
     use_oidc             = true
   }
 }
